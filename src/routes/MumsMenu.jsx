be8d5141f2ca5e3f003cc974b04c9dec69a6e4af
Felipe Fernandez <fettoviktig@gmail.com>
2024-04-05 08:41:22 +0200

    addMenuItem start

--- src/routes/MumsMenu.jsx
+++ src/routes/MumsMenu.jsx
@@ -1,8 +1,34 @@
+import MenuItemForm from "../components/atoms/MenuItemFormInput"
+import styled from "styled-components"
+import logo from "../assets/img/andra-longos-light-logo.svg";
+
+const StyledMumsMenu = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+	font-size: var(--font-med);
+	align-items: center;
+	justify-content: flex-start;
+	text-align: center;
+	padding: 20px;
+	box-sizing: border-box;
+`;
+
+const Logo = styled.img`
+	align-self: center;
+	width: 200px;
+	height: auto;
+	margin-top: 286px;
+	margin-bottom: 20px;
+`;
 const MumsMenu = () => (
-	<div>
-		<h2> Välkommen! </h2>
-		<p> PERSONALSIDANNANANAN </p>
-	</div>
+	<StyledMumsMenu>
+		<Logo src={logo} alt="logo" />
+		<MenuItemForm />
+
+	</StyledMumsMenu>
+	
 )
 
 export default MumsMenu
